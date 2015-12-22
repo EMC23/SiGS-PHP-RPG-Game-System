8f3b224abfc1fd024716d8e103eda5b5fde4929f
techbot <djtechbot@gmail.com>
2015-12-22 12:55:21 +0100

    whitespace

--- src/Emc23/SigsBundle/Model/Jigs.php
+++ src/Emc23/SigsBundle/Model/Jigs.php
@@ -1,73 +1,52 @@
 <?php
 
-
 namespace Emc23\SigsBundle\Model;
         
         use Doctrine\ORM\Query;
-
         use Emc23\SigsBundle\Entity\J17JigsCharacters;
-
-
 
 class Jigs extends DefaultController
 {
 
     function generateFaction()
     {
-
         $result = 42;
-
         return $result;
     }
 
     function generateGroup()
     {
-
         $result = 47;
-
         return $result;
     }
-    
-    
+
      function generateHealth()
     {
-
         $result = 10;
-
         return $result;
     }   
-    
-    
+
      function generateIntelligence()
     {
-
         $result = 10;
-
         return $result;
     }   
     
 
     function generateStrength()
     {
-
         $result = 10;
-
         return $result;
     }
     
-    
     function generateOwner($Gid,$em)
     {
-
-      $query = $em->createQuery(
-            ' SELECT a FROM Emc23SigsBundle:J17Characters a WHERE a.gid ='.$Gid);
+      $query = $em->createQuery('SELECT a FROM Emc23SigsBundle:J17Characters a
+      WHERE a.gid ='.$Gid);
         // print_r($query);
-
-            $x = $query->getResult(Query::HYDRATE_ARRAY);
-
-        $result = $x[0];
-
-        return $result;
+      $x = $query->getResult(Query::HYDRATE_ARRAY);
+      $result = $x[0];
+      return $result;
     }
     
 }
