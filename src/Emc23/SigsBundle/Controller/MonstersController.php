8a103555b35c629cee0468ff2c05484603390c23
techbot <djtechbot@gmail.com>
2016-01-04 15:56:09 +0100

    no pagination

--- src/Emc23/SigsBundle/Controller/MonstersController.php
+++ src/Emc23/SigsBundle/Controller/MonstersController.php
@@ -17,8 +17,7 @@ namespace Emc23\SigsBundle\Controller;
         use Symfony\Component\HttpFoundation\Request;
         use Doctrine\ORM\Query;
         // src/Acme/StoreBundle/Controller/DefaultController.php
-      
-
+        use Doctrine\ORM\Tools\Pagination\Paginator;
 class MonstersController extends Controller
 {
     public function __get($var)
@@ -103,10 +102,6 @@ class MonstersController extends Controller
             $strength = $task->getStrength();
             $gid = $task->getGid();
             $owner = $task->getOwner();
-
-            //$faction    = $task->getFaction();
-            //$gid        = $task->getGid();
-
             $task->setName($name);
             $task->setFaction($faction);
             $task->setHealth($health);
@@ -126,7 +121,6 @@ class MonstersController extends Controller
 
     public function showAction($id, $type, Request $request)
     {
-
         if ($type == 'J17JigsCharacters') {
             $task = new J17JigsCharacters();
         }
@@ -139,48 +133,20 @@ class MonstersController extends Controller
         if ($type == 'J17JigsHobbits') {
             $task = new J17JigsHobbits();
         }
-
         $record = $this->getDoctrine()
             //  ->getRepository('AcmeSigsBundle:J17JigsPlayers')
             ->getRepository("Emc23SigsBundle:$type")
             ->find($id);
-
         if (!$record) {
             throw $this->createNotFoundException('No record found for id ' . $id);
         }
-
-
-
-
-
         // ... do something, like pass the $product object into a template
 
-        // return $product;
-        //   return new Response('product id '.$product->getIdUser());
-
-
-        //    $faction = $record->getFaction();
-        // $image = $product->getImage();
-        //    $Posx = $product->getPosx();
-
-        //  $task = new J17JigsCharacters();
-
-//   $task = new $type();
-
         if ($type == 'J17JigsHobbits') {
-
-
-
-
-
-
             $name = $record->getName();
             $faction = $record->getFaction();
             $health = $record->getHealth();
             $strength = $record->getStrength();
-
-
-
             $intelligence = $record->getIntelligence();
            // $gid = $record->getGid();
             $owner = $record->getOwner();
@@ -205,225 +171,42 @@ class MonstersController extends Controller
                 ->add('contentment', 'text')
                 ->add('save', 'submit')
                 ->getForm();
-
         }
-
-
-
-
         return $this->render("Emc23SigsBundle:Default:" . $type . "_page.html.twig", array('stuff' => $record, 'form' => $form->createView(), 'type' => $type));
-
-
     }
 
-    ///////////////////////////////////////////////////////////////////
 
 
-    public function showfactionsAction()
-    {
+            public function listAction()
+            {
 
-        $gids = array(42, 35, 36);
-        $this->faction = array();
-        $em = $this->getDoctrine()->getManager();
-        /*
-              $query = $em->createQuery(
-                  'SELECT p
-                  FROM AcmeSigsBundle:J17Comprofiler p
-                  WHERE p.id > :id
-                  ORDER BY p.id ASC'
-              )->setParameter('id', '1');
+                $em         = $this->get('doctrine.orm.entity_manager');
+                $dql        = "SELECT a, b FROM Emc23SigsBundle:J17JigsMonsters a LEFT JOIN a.type b";
+                $query      = $em->createQuery($dql)
+                ->setFirstResult(0)
+                ->setMaxResults(100);
 
-              $x = $query->getResult();
-          */
-        foreach ($gids as $gid) {
-            //   $dql   = "           SELECT a,p FROM AcmeSigsBundle:J17Usergroups a             LEFT JOIN a.gid p WHERE a.parentId = $gid ";
+                //print_r($query);
+                //exit();
 
-            /*    $query = $em->createQuery(
-                'SELECT p FROM AcmeSigsBundle:J17Usergroups p
-                 WHERE p.parentId = 42'
 
-            );  */
+                $repository = $this->getDoctrine()
+                    ->getRepository('Emc23SigsBundle:J17JigsMonsters');
 
-            $query = $em->createQuery(' SELECT p,a FROM Emc23SigsBundle:J17Usergroups a  LEFT JOIN a.gid p WHERE a.parentId =' . $gid);
 
-            // print_r($query);
+                // find *all* products
+                $monsters = $repository->findAll();
 
-            $x = $query->getResult(Query::HYDRATE_ARRAY);
 
-            //    $x = $query->getResult();
+                foreach ($monsters as $monster) {
+                    $monster->name          = $monster->getType()->getname();
+                    $monster->cellwidth     = $monster->getType()->getCellwidth();
+                    $monster->cellheight    = $monster->getType()->getCellheight();
+                }
 
-            $this->faction[$gid] = $x;
-            //
-            $this->faction[$gid]['factiontotalBank'] = 0;
-            //
-            foreach ($x as $group) {
-
-                $this->faction[$gid]['factiontotalBank'] += $group['gid']['totalBank'];
-            }
-            $i = 0;
-            foreach ($this->faction as $page) {
-                //   echo $this->faction[$gid]['factiontotalBank']. '<br/>';
-                //    echo $page[$i]['gid']['totalBank'] . 'tb<br/>';
-                //   echo $i. '<br/>';
-                //   echo $this->$gid->faction_totalMoney.'<br/>';
+                //$paginator  = $this->get('knp_paginator');
+                //$pagination = $paginator->paginate($query, $this->get('request')->query->get('page', 1)/*page number*/, 30/*limit per page*/);
                 // parameters to template
-                //       echo '<pre>';
-                // print_r ($page['gid']['totalBank'] );
-                //   print_r($page);
-
-
-                if ($page[$i]['parentId'] == 42) {
-                    $this->faction[$gid]['fid'] = "Cyberia";
-                }
-                if ($page[$i]['parentId'] == 35) {
-                    $this->faction[$gid]['fid'] = "Gaia";
-                }
-
-                if ($page[$i]['parentId'] == 36) {
-
-                    $this->faction[$gid]['fid'] = "Fantasia";
-                }
-
-
-                $i++;
-            }
-
-
-//  
-        }
-
-
-// return $this->render('AcmeSigsBundle:Default:layout.html.php');
-        return $this->render('Emc23SigsBundle:Default:layout.html.php', array('pagination' => $this->faction));
-
-        // ... do something, like pass the $product object into a template
-
-        // return $product;
-        //   return new Response('product id '.$product->getIdUser());
-
-        // $name = $product->getName();
-        //    $image = $product->getImage();
-        //  $Posx = $product->getPosx();
-
-        //$task = new J17JigsCharacters();
-        //
-        //     $task->setName($name);
-        //     $task->setImage($image);
-        //     $task->setPosx($Posx);
-
-
-        //      $form = $this->createFormBuilder($task)
-        //        ->add('name', 'text')
-        //        ->add('image', 'text')
-        //      ->add('posx','text')
-        //           ->add('save', 'submit')
-        //
-//            ->getForm();
-        //    return $this->render("AcmeSigsBundle:Default:J17_Jigs_Factions_page.html.twig", array('stuff' => $products, 'form' => $form->createView() ));
-
-
-    }
-
-/////////////////////////////////          
-
-            public function showallusersAction()
-            {
-                $em = $this->getDoctrine()->getManager();
-                $query = $em->createQuery(
-                    'SELECT p
-            FROM Emc23SigsBundle:J17JigsPlayers p
-            WHERE p.id > :id
-            ORDER BY p.iduser ASC'
-                )->setParameter('id', '1');
-
-                $products = $query->getResult();
-                $x = 0;
-                $content = '<table>';
-                foreach ($products as $record) {
-
-                    $content .= $this->renderView(
-                        'Emc23SigsBundle:Hello:index.html.twig',
-                        array('name' => $record->getName(), 'id' => $record->getId())
-                    );
-                }
-                $content .= '</table>';
-                return new Response($content);
-            }
-
-            ////////////////////////////////////////////////////////////////////////////////////
-            public function showGroupsAction()
-            {
-                $content = "empty";
-                return new Response($content);
-            }
-
-/////////////////////////////////
-
-            public function showallusers2Action()
-            {
-                $em = $this->getDoctrine()->getManager();
-                $query = $em->createQuery('SELECT p FROM Emc23SigsBundle:J17Comprofiler p WHERE p.id > 1 ORDER BY p.id ASC');
-                //$query->setMaxResults(1);
-                $thing = $query->getSql();
-
-               // echo $thing;
-
-
-                try {
-                    // default action is always to return a Document
-
-                    echo '<pre>';
-                    \Doctrine\Common\Util\Debug::dump($query);
-                    echo '</pre>';
-
-                 //   $products = $query->getResult();
-
-
-
-                    $document = $query->getResult(Query::HYDRATE_SCALAR);
-                } catch (QueryException $e){
-                    print_r($e);
-                    // no result or non unique result
-                    }
-
-
-
-
-
-                $x = 0;
-                $content = '<table>';
-                foreach ($products as $record) {
-
-                    $content .= $this->renderView(
-                        'AcmeSigsBundle:Default:index3.html.twig',
-                        array('image' => $record->getAvatar(), 'id' => $record->getId())
-                    );
-                }
-                $content .= '</table>';
-                return new Response($content);
-            }
-            ////////////////////////////////////////////////////////////////////////////////////
-
-
-            // Acme\MainBundle\Controller\ArticleController.php
-
-            public function listAction($type)
-            {
-                $em = $this->get('doctrine.orm.entity_manager');
-                $dql = "SELECT a FROM Emc23SigsBundle:$type a";
-
-                $query = $em->createQuery($dql);
-
-
-                $paginator = $this->get('knp_paginator');
-                $pagination = $paginator->paginate($query, $this->get('request')->query->get('page', 1)/*page number*/, 30/*limit per page*/);
-                // $pagination ="hello";
-
-
-                //  echo '<pre>';
-                //  print_r ($pagination);
-                //  echo '</pre>';
-                // parameters to template
-                return $this->render('Emc23SigsBundle:Default:' . $type . '.html.twig', array('pagination' => $pagination, 'type' => $type));
+                return $this->render('Emc23SigsBundle:Default:J17JigsMonsters.html.twig', array('pagination' => $monsters));
             }
         }
