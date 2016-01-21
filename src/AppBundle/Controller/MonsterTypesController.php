b293068d1fa6f83af992b0c875a7b6f73e72bf09
techbot <djtechbot@gmail.com>
2016-01-21 22:45:02 +0100

    save action

--- src/AppBundle/Controller/MonsterTypesController.php
+++ src/AppBundle/Controller/MonsterTypesController.php
@@ -10,6 +10,8 @@ use Doctrine\ORM\Tools\Pagination\Paginator;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/monstertypes")
@@ -43,7 +45,7 @@ class MonsterTypesController extends Controller
         $task = new J17JigsMonsterTypes();
         $record = $this->getDoctrine()
             //  ->getRepository('AcmeSigsBundle:J17JigsPlayers')
-            ->getRepository("AppBundle:$type")
+            ->getRepository("AppBundle:J17JigsMonstertypes")
             ->find($id);
 
         if (!$record) {
@@ -68,5 +70,58 @@ class MonsterTypesController extends Controller
 
         return $this->render("default/J17JigsMonsterTypes_page.html.twig", array('stuff' => $record, 'form' => $form->createView()));
 
+    }
+
+
+    /**
+     * @Route("/api/update")
+     */
+    public function saveAction(Request $request)
+    {
+        foreach ($request->get('data') as $key=>$value){
+            $id         = $key;
+        }
+
+
+        $em         = $this->getDoctrine()->getManager();
+        $model      = $this->get('monsterTypesModel');
+        $task       = $this->getDoctrine()
+            ->getRepository("AppBundle:J17JigsMonsterTypes")
+            ->find($id);
+
+
+        if (!$task) {
+            throw $this->createNotFoundException('No record found for id ' . $id);
+        }
+        $model->save($request, $task, $em);
+
+        $record = $this->getDoctrine()
+            ->getRepository("AppBundle:J17JigsMonsterTypes")
+            ->find($id);
+
+
+
+        $response = new Response(json_encode(array('data' => $model->repopulate($record))));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    /**
+     * @Route("/api/list", name="emc23_sigs_monsterTypes_api")
+     */
+    public function apiListAction($start=0,$max=1000)
+    {
+        $em         = $this->get('doctrine.orm.entity_manager');
+        $dql        = "SELECT a FROM AppBundle:J17JigsMonsterTypes a";
+        $query      = $em->createQuery($dql)
+            ->setFirstResult($start)
+            ->setMaxResults($max);
+        $resource   = $query->getResult();
+        $model      = $this->get('MonsterTypesModel');
+        $result     = $model->repopulate($resource);
+
+        // create a JSON-response with a 200 status code
+        $response   = new Response(json_encode(array('data' => $result)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 }
