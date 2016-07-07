22dd29f8609dabf17a6f8d8e61185715b9320a1d
techbot <djtechbot@gmail.com>
2016-07-07 15:29:25 +0200

    knp paginator

--- src/AppBundle/Menu/Builder.php
+++ src/AppBundle/Menu/Builder.php
@@ -1,50 +1,50 @@
 <?php
 // src/Acme/DemoBundle/Menu/Builder.php
-namespace Acme\HelloBundle\Menu;
+namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 
-
-
-class Builder
-
-//class Builder extends ContainerAware
+class Builder implements ContainerAwareInterface
 {
+    use ContainerAwareTrait;
+
     public function mainMenu(FactoryInterface $factory, array $options)
     {
         $menu = $factory->createItem('root');
 $menu->setChildrenAttributes(array('id' => 'menu'));
-        $menu->addChild('Home', array('route' => 'acme_hello_homepage4'));
+
+        $menu->addChild('Home', array('route' => 'homepage'));
         
         $menu->addChild('Players', array(
-            'route' => 'acme_hello_homepage5',
+            'route' => 'list',
             'routeParameters' => array('type' => 'J17JigsPlayers')
         ));
         
         $menu->addChild('Buildings', array(
-            'route' => 'acme_hello_homepage5',
+            'route' => 'list',
             'routeParameters' => array('type' => 'J17JigsBuildings')
         ));       
         
         $menu->addChild('Characters', array(
-            'route' => 'acme_hello_homepage5',
+            'route' => 'list',
             'routeParameters' => array('type' => 'J17JigsCharacters')        
          ));
          
         $menu->addChild('Factions', array(
           //  'route' => 'acme_hello_homepage7'
-            'route' => 'acme_hello_homepage5',
+            'route' => 'list',
             'routeParameters' => array('type' => 'J17JigsFactions')         
          ));
                   
          $menu->addChild('Groups', array(
-            'route' => 'acme_hello_homepage8',
+            'route' => 'list',
             'routeParameters' => array('type' => 'J17JigsGroups')        
          ));       
          
          $menu->addChild('Hobbits', array(
-            'route' => 'acme_hello_homepage5',
+            'route' => 'list',
             'routeParameters' => array('type' => 'J17JigsHobbits')        
          ));        
          
